839932ba6b3249ca1bf8eff3f85ded9d0b614de6
Sebastian Chmielewski <s.chmielewski@oberthur.com>
2018-09-06 18:11:03 +0200

    Swagger documentation

--- app/src/main/java/com/docker/atsea/AtSeaApp.java
+++ app/src/main/java/com/docker/atsea/AtSeaApp.java
@@ -6,18 +6,28 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.docker.atsea.configuration.JpaConfiguration;
 import com.docker.atsea.controller.LoginController;
 import com.docker.atsea.security.JwtFilter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger1.annotations.EnableSwagger;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @Import(JpaConfiguration.class)
 @SpringBootApplication(scanBasePackages={"com.docker.atsea"})
 @EntityScan("com.docker.atsea.model")
 @EnableJpaRepositories("com.docker.atsea.repository")
+@EnableSwagger2
+@EnableSwagger
+@Configuration
 public class AtSeaApp {
 
 	@Bean
@@ -29,7 +39,16 @@ public class AtSeaApp {
 
         return registrationBean;
     }
-	
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(AtSeaApp.class, args);
 	}
--- app/src/main/java/com/docker/atsea/configuration/WebConfiguration.java
+++ app/src/main/java/com/docker/atsea/configuration/WebConfiguration.java
@@ -15,6 +15,11 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**").addResourceLocations("file:/static/");
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 	
 	@Override
